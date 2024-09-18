3d5364f77a540693faaf2811d4156d6b6d180e6e
339713273 <339713273@2260I-L29-026>
2024-09-18 09:09:59 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Topic_4_pratice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Double price;
+            Console.WriteLine("what is the price");
+            double.TryParse(Console.ReadLine(), out price);
+            Console.WriteLine("the price is " + (price / 5).ToString("c"));
+
+
+
+
+            
+        }
+    }
+}
